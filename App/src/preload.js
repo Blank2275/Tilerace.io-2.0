66b59ce8cc8421e99e9939d31c3be9461179813b
Blank2275 <cfr3.14156@gmail.com>
2022-02-06 11:39:22 -0700

    added qr code

--- App/src/preload.js
+++ App/src/preload.js
@@ -1,4 +1,16 @@
 const {dialog, ipcRenderer} = require('electron');
+const QRCode = require('qrcode');
+const ip = require('ip')
+
+window.addEventListener('DOMContentLoaded', () => {
+    var qrcanvas = document.getElementById('qrcode');
+    //var ip = os.networkInterfaces()['venet0:0'][0].address;
+    var address = ip.address();
+    QRCode.toCanvas(qrcanvas, `http://${address}:8080`, (err) => {
+        if(err) console.error(err);
+        console.log('succesfully made qr code')
+    })
+})
 
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('submit').addEventListener('click', (e) => restart(e))
